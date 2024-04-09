ca1afc85710fb50daa45a858201182b28ef453c8
yngrid vitoria <145464353+yngridv@users.noreply.github.com>
2024-04-09 11:44:05 -0300

    Add files via upload

---
+++ sistemaaluno/testealuno.php
@@ -0,0 +1,16 @@
+
+    <?php 
+    var_dump ($_GET);
+    echo " o meu nome é" . $_GET['nome'];
+    echo "<br>";
+    echo " o endereço digitado foi" . $_GET['endereco'];
+    echo "<br>";
+    echo " minha data de nascimento é" . $_GET['date'];
+    echo "<br>";
+    echo " minha senha é" . $_GET['senha'];
+    echo "<br>";
+    echo " meu email é" . $_GET['email'];
+ ?>
+    
+ 
+    
